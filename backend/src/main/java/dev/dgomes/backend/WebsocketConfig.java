4b578138182577349c406bb10de835950f6112b7
Diogo Gomes <diogo.gomes77@gmail.com>
2020-05-10 10:37:45 +0100

    replaced deprecated AbstractWebSocketMessageBrokerConfigurer

--- backend/src/main/java/dev/dgomes/backend/WebsocketConfig.java
+++ backend/src/main/java/dev/dgomes/backend/WebsocketConfig.java
@@ -2,14 +2,13 @@ package dev.dgomes.backend;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebsocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
