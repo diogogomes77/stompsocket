2dc0d2726bb356f5cb74b8d298b9dc69a7213dba
Diogo Gomes <diogo.gomes77@gmail.com>
2020-05-16 14:05:11 +0100

    removed fileupload package

--- backend/src/main/java/dev/dgomes/backend/BackendApplication.java
+++ backend/src/main/java/dev/dgomes/backend/BackendApplication.java
@@ -1,15 +1,9 @@
 package dev.dgomes.backend;
 
-import dev.dgomes.backend.fileupload.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		//FileStorageProperties.class,
-		StorageProperties.class
-})
 public class BackendApplication {
 
 	public static void main(String[] args) {
